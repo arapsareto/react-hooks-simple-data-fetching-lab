7c60e7887d8bc6fb6e669359b03c708c4ea34395
martinsareto <martin.kiprotich@student.moringaschool.com>
2022-07-30 00:15:22 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React from "react";
+import { useState, useEffect } from "react";
+
+function App(){
+
+    const[respData, setRespData] = useState("")
+    const[apiLoading, setApiLoading] = useState(true)
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((resp)=>resp.json())
+        .then((data)=>{
+            setRespData(data.message)
+            setApiLoading(false)
+        })
+    },[])
+
+    if(apiLoading) return <p>Loading....</p>
+
+return(
+    <div>
+        <img src={respData} alt="random dog"></img>
+    </div>
+)
+}
+export default App;   
